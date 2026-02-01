dbb689bc7a702ea86a39d935683811ce90d8e846
Oscar V <oscarvalenzuelab@users.noreply.github.com>
2026-01-31 18:30:05 -0800

    Release v1.6.3 (#66)
    * chore: Release v1.6.3
    
    Version bump to 1.6.3 including:
    - Fix for invalid SPDX license suffixes (#64)
    - Fix for BlueOak license detection (#63)
    
    Updated:
    - osslili/__init__.py: version 1.6.2 -> 1.6.3
    - pyproject.toml: version 1.6.2 -> 1.6.3
    - CHANGELOG.md: added v1.6.3 release notes
    
    * Add template files for standardization
    
    - Add .clabot for CLA enforcement
    - Add AUTHORS.md for contributor attribution
    - Add CODE_OF_CONDUCT.md for community standards
    - Add CHANGELOG.md for version tracking
    
    Part of SEMCL.ONE organization-wide standardization.

--- osslili/__init__.py
+++ osslili/__init__.py
@@ -13,7 +13,7 @@ if os.environ.get('OSLILI_DEBUG') != '1':
     except ImportError:
         pass
 
-__version__ = "1.6.2"
+__version__ = "1.6.3"
 
 from .core.generator import LicenseCopyrightDetector
 from .core.models import (
